57fda287776ffcdf59a0af5f5b681c7d001aa1de
prantikpariksha <46423877+prantikpariksha@users.noreply.github.com>
2019-01-29 18:39:49 +0530

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,32 @@
+"""
+This includes three functions.When someone calls these functions
+it will return the corresponding value defined under the three 
+global variables
+"""
+
+Artist="Bob Marley"                  #This is the singer name
+Title="No Woman No Cry"              #This is the Song name
+Album="Raga Music" 
+var1=True
+var2=False                  #this is the Album name
+
+def GetArtistName(name):
+    return name
+ArtistName=GetArtistName(Artist)
+print(ArtistName)
+
+def GetTitleName(name):
+    return name
+TitleName=GetTitleName(Title)
+print(TitleName)
+
+def GetAlbumName(name):
+    return name
+AlbumName=GetAlbumName(Album)
+print(AlbumName)
+
+def Booleans(name):
+    return name
+var3=Booleans(var1)
+var4=Booleans(var2)
+print (var3,var4)
