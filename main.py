3f797b0c040607ac3ea0829331a4901aeacb23a6
prantikpariksha <46423877+prantikpariksha@users.noreply.github.com>
2019-01-29 13:24:38 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,67 @@
+#This file contains information about few songs those are my favourite
+
+
+Artist="Bob Marley"                  #This is the singer name
+Title="No Woman No Cry"              #This is the Song name
+Album="Raga Music"                   #this is the Album name
+TrackLength=7.58                     #this is the duration of the track
+Genre="Raggae"                       #this is the type of the track
+TrackNumber=7                        #this is the song sequence number
+BitDepth="16 Bit"                    #this defines quality of the song
+SamplingRateInKHz=44                 #this is the number of samples of audio carried per second
+SizeInMB=6.8                         #this is the file size
+
+print(Artist)
+print(Title)
+print(Album)
+print(TrackLength)
+print(Genre)
+print(TrackNumber)
+print(BitDepth)
+print(SamplingRateInKHz)
+print(SizeInMB)
+print("******************************")
+print("\n")
+
+Artist="The Doors"                  
+Title="The End"
+Album="The best of the Doors"
+TrackLength=11.46
+Genre="Rock"
+TrackNumber=4
+BitDepth="32 Bit"
+SamplingRateInKHz=48
+SizeInMB=11.5
+
+print(Artist)
+print(Title)
+print(Album)
+print(TrackLength)
+print(Genre)
+print(TrackNumber)
+print(BitDepth)
+print(SamplingRateInKHz)
+print(SizeInMB)
+print("******************************")
+print("\n")
+
+Artist="Pink Floyd"                  
+Title="Another brick in the wall"
+Album="The Wall"
+TrackLength=7.8
+Genre="Rock"
+TrackNumber=7
+BitDepth="32 Bit"
+SamplingRateInKHz=48
+SizeInMB=8.5
+
+print(Artist)
+print(Title)
+print(Album)
+print(TrackLength)
+print(Genre)
+print(TrackNumber)
+print(BitDepth)
+print(SamplingRateInKHz)
+print(SizeInMB)
+print("******************************")
